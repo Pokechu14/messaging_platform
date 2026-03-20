f56012018e2eb2b9b18d2ed01126d48a91f811ed
kalapimea <atsukka2011@gmail.com>
2026-03-20 17:16:32 +0200

    Refactored code and added threading to support multiple clients at once

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import socket
 import threading
 
 clients = {}
+clients_lock = threading.Lock()
+
 
 #initialize server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,19 +17,40 @@ server.listen()
 def add_client():
     client, addr = server.accept()
     client_name = client.recv(1024).decode("utf-8")
-    clients[client_name] = client   #clients[example].send("example".encode("utf-8"))
+    with clients_lock:
+        clients[client_name] = client   #clients[example].send("example".encode("utf-8"))
     print(f"Succesfully connected to {client_name}!")
+    return client, client_name
 
 def handle_client(client_socket, client_name):
     while True:
         message = client_socket.recv(1024).decode("utf-8")
         if message == "":
-            del clients[client_socket]
+            with clients_lock:
+                if client_name in clients:
+                    del clients[client_name]
             break
         
-        for socket in clients.values():
+
+        with clients_lock:
+            sockets = list(clients.values())
+        
+        for socket in sockets:
             socket.send(f"{client_name}: {message}".encode("utf-8"))
     
+
+running = True
+while running:
+    client, client_name = add_client()
+
+    threading.Thread(
+        target=handle_client,
+        args=(client, client_name),
+        daemon=True
+    )
+
+
+
 
 
 
