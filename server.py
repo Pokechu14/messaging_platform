0669a5e5e484215f6c2c34318bf98273e17d931e
Pokechu14 <samu.ryynanen@oppilas.kontiolahti.fi>
2026-03-25 12:11:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 import socket
 import threading
+import datetime
 
 clients = {}
 clients_lock = threading.Lock()
 
-
+servername = socket.gethostname()
+serverIP = socket.gethostbyname(servername)
 #initialize server
+#aten ip 192.168.100.12
+#aten portti 9090
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = "192.168.100.12"
+HOST = "serverIP"
 PORT = 9090
 
 server.bind((HOST, PORT))
@@ -24,7 +28,7 @@ def add_client():
 
 def handle_client(client_socket, client_name):
     while True:
-        message = client_socket.recv(1024).decode("utf-8")
+        message = client_socket.recv(1024).decode("utf-16")
         if message == "":
             with clients_lock:
                 if client_name in clients:
@@ -37,7 +41,7 @@ def handle_client(client_socket, client_name):
         
         for socket in sockets:
             if socket != client_socket:
-                socket.send(f"{client_name}: {message}".encode("utf-8"))
+                socket.send(f"{client_name}: {message}".encode("utf-16"))
     
 
 running = True
