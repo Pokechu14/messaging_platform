0a8a735c696ce97745e211ee30c698bf5dcbf3fb
Pokechu14 <samu.ryynanen@oppilas.kontiolahti.fi>
2026-03-25 13:42:24 +0200

    Update client.py
    remade code

--- client.py
+++ client.py
@@ -1,22 +1,32 @@
 import socket
 import threading
+import sys
+import datetime
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(("192.168.100.12", 9090))
+
+clientpcname = socket.gethostname()
+clientIP = socket.gethostbyname(clientpcname)
+serverIP = input("Enter server ip: ")
+serverport = input("Enter server port: ")
+client.connect((serverIP, serverport))
 
 done = False
 
-client_name = input("Enter the name of your client: ")
-client.send(client_name.encode("utf-8"))
-
+client_name = input("Enter username: ")
+client.send(client_name.encode("utf-16"))
+print("press up arrow to enter message")
+if (event.type == KEYUP):
+    messagetosend = input("Enter message ")
 def send_message():
-    client.send(input().encode("utf-8"))
+    client.send(f"{messagetosend} --- sent {now} by {client_name} ".encode("utf-16"))
 
 def recieve_message():
-    print(client.recv(1024).decode("utf-8"))
+    print(client.recv(3024).decode("utf-16"))
 
 
 while not done:
+    now = datetime.datetime.now()
     threading.Thread(target=send_message).start()
     threading.Thread(target=recieve_message).start()
